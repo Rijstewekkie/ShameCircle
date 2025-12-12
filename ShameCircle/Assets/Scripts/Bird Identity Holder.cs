69f1ee92eada3d328e9357a4039a2c955bd1332c
TPODanzig <tpodanzig@gmail.com>
2025-12-12 13:12:49 +0100

    smol changes

--- ShameCircle/Assets/Scripts/Bird Identity Holder.cs
+++ ShameCircle/Assets/Scripts/Bird Identity Holder.cs
@@ -39,30 +39,6 @@ public class BirdIdentityHolder : MonoBehaviour
             Debug.LogError("BirdType is NULL, Deleting object");
             Destroy(this.gameObject);
         }
-
-        if (BirdType == BirdTypes.EurasianRobin)
-        { 
-            RandomChance = Random.Range(0, 100);
-            if (RandomChance == 69)
-            {
-                IgnoreAsset = Resources.Load($"IgnoreThis") as GameObject;
-
-                if (IgnoreAsset != null)
-                {
-                    GameObject asset = Instantiate(IgnoreAsset, transform.position, transform.rotation, transform);
-                    
-                    asset.layer = gameObject.layer;
-                    foreach (Transform child in asset.transform.GetComponentsInChildren<Transform>())
-                    {
-                        child.gameObject.layer = gameObject.layer;
-                    }
-                }
-                else
-                {
-                    Debug.Log("easteregg popped");
-                }   
-            }
-        }
         
         if (SceneManager.GetActiveScene().buildIndex == 1 && !scene1Birds.Contains((int)BirdType))
         {
